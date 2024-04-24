0282bf4456a984e5d2df11e68f51d6082ad929f3
Haris Ibragic <ibragicharis01@gmail.com>
2024-04-24 18:39:23 +0200

    create randomNumber function

--- src/Review.js
+++ src/Review.js
@@ -174,7 +174,13 @@ const Review = () => {
     });
   };
 
-  const randomPerson = () => {};
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  };
 
   return (
     <article className='review'>
