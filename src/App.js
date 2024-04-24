956e3f7deac4fe10a86de11db36b7a41d96dc053
Haris Ibragic <ibragicharis01@gmail.com>
2024-04-24 17:54:54 +0200

    create app component

---
+++ src/App.js
@@ -0,0 +1,42 @@
+// import React from 'react';
+// import Review from './Review';
+// function App() {
+//   return (
+//     <main>
+//       <section className='container'>
+//         <div className='title'>
+//           <h2>our Reviews</h2>
+//           <div className='underline'></div>
+//         </div>
+//         <Review />
+//       </section>
+//     </main>
+//   );
+// }
+
+// export default App;
+// import React from 'react';
+// import Review from './Review';
+
+// function App() {
+//   return (
+//     <main>
+//       <section className='container'>
+//         <div className='title'>
+//           <h2>Our reviews</h2>
+//           <div className='underline'></div>
+//         </div>
+//         <Review />
+//       </section>
+//     </main>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import Review from './Review';
+
+function App() {}
+
+export default App;
---
+++ src/Review.js
@@ -0,0 +1,143 @@
+// import React, { useState } from 'react';
+// import people from './data';
+// import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+// const Review = () => {
+//   const [index, setIndex] = useState(0);
+//   const { name, job, image, text } = people[index];
+
+//   const checkNumber = number => {
+//     if (number > people.length - 1) {
+//       return 0;
+//     }
+//     if (number < 0) {
+//       return people.length - 1;
+//     }
+
+//     return number;
+//   };
+
+//   const nextPerson = () => {
+//     setIndex(curIndex => {
+//       let newIndex = curIndex + 1;
+//       return checkNumber(newIndex);
+//     });
+//   };
+
+//   const prevPerson = () => {
+//     setIndex(curIndex => {
+//       let newIndex = curIndex - 1;
+//       return checkNumber(newIndex);
+//     });
+//   };
+
+//   const randomPerson = () => {
+//     // 3.3 -> 3 - ceil
+//     // 3.3 -> 4 - floor
+//     let randomNumber = Math.trunc(Math.random() * people.length);
+//     let randomNbr = Math.trunc(Math.random() * people.length);
+//     console.log(randomNbr);
+//     if (randomNumber === index) {
+//       randomNumber = index + 1;
+//     }
+//     setIndex(checkNumber(randomNumber));
+//   };
+
+//   return (
+//     <article className='review'>
+//       <div className='img-container'>
+//         <img src={image} alt={name} className='person-img' />
+//         <span className='quote-icon'>
+//           <FaQuoteRight />
+//         </span>
+//       </div>
+//       <h4 className='author'>{name}</h4>
+//       <p className='job'>{job}</p>
+//       <p className='info'>{text}</p>
+//       <div className='button-container'>
+//         <button className='prev-btn' onClick={prevPerson}>
+//           <FaChevronLeft />
+//         </button>
+//         <button className='next-btn' onClick={nextPerson}>
+//           <FaChevronRight />
+//         </button>
+//         <button className='random-btn' onClick={randomPerson}>
+//           Random Person
+//         </button>
+//       </div>
+//     </article>
+//   );
+// };
+
+// export default Review;
+
+// import React, { useState } from 'react';
+// import people from './data';
+// import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+// const Review = () => {
+//   const [index, setIndex] = useState(0);
+//   const { name, job, image, text } = people[index];
+
+//   const checkNumber = number => {
+//     if (number > people.length - 1) {
+//       return 0;
+//     }
+//     if (number < 0) {
+//       return people.length - 1;
+//     }
+//     return number;
+//   };
+
+//   const prevPerson = () => {
+//     setIndex(curIndex => {
+//       let changePerson = curIndex - 1;
+//       return checkNumber(changePerson);
+//     });
+//   };
+
+//   const nextPerson = () => {
+//     setIndex(curIndex => {
+//       let changePerson = curIndex + 1;
+//       return checkNumber(changePerson);
+//     });
+//   };
+
+//   const randomPerson = () => {
+//     let randomNumber = Math.floor(Math.random() * people.length);
+//     if (randomNumber === index) {
+//       randomNumber = index + 1;
+//       setIndex(checkNumber(randomNumber));
+//     }
+//   };
+
+//   return (
+//     <article className='review'>
+//       <div className='img-container'>
+//         <img src={image} alt={name} className='person-img' />
+//         <span className='quote-icon'>
+//           <FaQuoteRight />
+//         </span>
+//       </div>
+//       <h4 className='author'>{name}</h4>
+//       <p className='job'>{job}</p>
+//       <p className='info'>{text}</p>
+
+//       <div className='button-container'>
+//         <button className='prev-btn' onClick={prevPerson}>
+//           <FaChevronLeft />
+//         </button>
+
+//         <button className='next-btn' onClick={nextPerson}>
+//           <FaChevronRight />
+//         </button>
+
+//         <button className='random-btn' onClick={randomPerson}>
+//           Random
+//         </button>
+//       </div>
+//     </article>
+//   );
+// };
+
+// export default Review;
