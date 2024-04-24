4114650cc63c9a8d3ff26d01e4b29218e9a3fcea
Haris Ibragic <ibragicharis01@gmail.com>
2024-04-24 18:09:28 +0200

    create information of people

--- src/Review.js
+++ src/Review.js
@@ -142,11 +142,27 @@
 
 // export default Review;
 
-import React from 'react';
+import React, { useState } from 'react';
 import people from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <div>Review</div>;
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='text'>{text}</p>
+    </article>
+  );
 };
 
 export default Review;
